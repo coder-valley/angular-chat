56ee2d410d750107fbcc6761b25386149500b866
Ashutosh Singh <ashu5oct1994@gmail.com>
2019-01-01 16:42:32 +0530

    chatbox component

--- src/app/chat/chatbox/chatbox.component.ts
+++ src/app/chat/chatbox/chatbox.component.ts
@@ -9,66 +9,43 @@ import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-chat-box',
-  templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.css'],
+  templateUrl: './chatbox.component.html',
+  styleUrls: ['./chatbox.component.css'],
   providers: [SocketService]
 })
 
-export class ChatBoxComponent implements OnInit{
+export class ChatBoxComponent implements OnInit {
 
-  @ViewChild('scrollMe', { read: ElementRef }) 
-  
-  public scrollMe: ElementRef;
-
-  
 
   public authToken: any;
   public userInfo: any;
-  public userList: any = [];
-  public disconnectedSocket: boolean;  
-
-  public scrollToChatTop:boolean= false;
-
   public receiverId: any;
   public receiverName: any;
-  public previousChatList: any = [];
-  public messageText: any; 
-  public messageList: any = []; // stores the current message list display in chat box
-  public pageValue: number = 0;
-  public loadingPreviousChat: boolean = false;
+  public userList: any = [];
+  public disconnectedSocket: boolean;
 
 
-
-  constructor( public AppService: AppService, public SocketService: SocketService, public router: Router, private toastr: ToastrService, public cookieService: CookieService) {}
-
-  ngOnInit() 
+  constructor( public AppService: AppService, public SocketService: SocketService, public router: Router, private toastr: ToastrService, public cookieService: CookieService ) 
   {
+    this.receiverId = this.cookieService.get('receiverId');
+    this.receiverName = this.cookieService.get('receiverName');
+  }
+
+  ngOnInit() {
 
     this.authToken = this.cookieService.get('authtoken');
 
     this.userInfo = this.AppService.getUserInfoFromLocalstorage();
 
-    this.receiverId = this.cookieService.get("receiverId");
-
-    this.receiverName =  this.cookieService.get('receiverName');
-
-    console.log(this.receiverId,this.receiverName)
-
-    if(this.receiverId!=null && this.receiverId!=undefined && this.receiverId!=''){
-      this.userSelectedToChat(this.receiverId,this.receiverName)
-    }
-
     this.checkStatus();
 
     this.verifyUserConfirmation();
-    this.getOnlineUserList()
 
-    this.getMessageFromAUser()
+    this.getOnlineUserList()
 
 
 
   }
-
 
   public checkStatus: any = () => {
 
@@ -96,6 +73,7 @@ export class ChatBoxComponent implements OnInit{
         this.disconnectedSocket = false;
 
         this.SocketService.setUser(this.authToken);
+        this.getOnlineUserList()
 
       });
     }
@@ -115,190 +93,8 @@ export class ChatBoxComponent implements OnInit{
 
         }
         
-        console.log(this.userList);
+        // console.log(this.userList);
 
       }); // end online-user-list
   }
-
-  // chat related methods 
-
-
-  public getPreviousChatWithAUser :any = ()=>{
-    // let previousData = (this.messageList.length > 0 ? this.messageList.slice() : []);
-    
-    // this.SocketService.getChat(this.userInfo.userId, this.receiverId, this.pageValue * 10)
-    // .subscribe((apiResponse) => {
-
-    //   console.log(apiResponse);
-
-    //   if (apiResponse.status == 200) {
-
-    //     this.messageList = apiResponse.data.concat(previousData);
-
-    //   } else {
-
-    //     this.messageList = previousData;
-    //     this.toastr.warning('No Messages available')
-
-       
-
-    //   }
-
-    //   this.loadingPreviousChat = false;
-
-    // }, (err) => {
-
-    //   this.toastr.error('some error occured')
-
-
-    // });
-
-  }// end get previous chat with any user
-
-
-  public loadEarlierPageOfChat: any = () => {
-
-    this.loadingPreviousChat = true;
-
-    this.pageValue++;
-    this.scrollToChatTop = true;
-
-    this.getPreviousChatWithAUser() 
-
-  } // end loadPreviousChat
-
-  public userSelectedToChat: any = (id, name) => {
-
-    console.log("setting user as active") 
-
-    // setting that user to chatting true   
-    this.userList.map((user)=>{
-        if(user.userId==id){
-          user.chatting=true;
-        }
-        else{
-          user.chatting = false;
-        }
-    })
-
-    this.cookieService.set('receiverId', id);
-
-    this.cookieService.set('receiverName', name);
-
-
-    this.receiverName = name;
-
-    this.receiverId = id;
-
-    this.messageList = [];
-
-    this.pageValue = 0;
-
-    let chatDetails = {
-      userId: this.userInfo.userId,
-      senderId: id
-    }
-
-
-    // this.SocketService.markChatAsSeen(chatDetails);
-
-    this.getPreviousChatWithAUser();
-
-  } // end userBtnClick function
-
-
-
-
-
-
-  public sendMessageUsingKeypress: any = (event: any) => {
-
-    if (event.keyCode === 13) { // 13 is keycode of enter.
-
-      this.sendMessage();
-
-    }
-
-  } // end sendMessageUsingKeypress
-
-  public sendMessage: any = () => {
-
-    if(this.messageText){
-
-      let chatMsgObject = {
-        senderName: this.userInfo.firstName + " " + this.userInfo.lastName,
-        senderId: this.userInfo.userId,
-        receiverName: this.cookieService.get('receiverName'),
-        receiverId: this.cookieService.get('receiverId'),
-        message: this.messageText,
-        createdOn: new Date()
-      } // end chatMsgObject
-      console.log(chatMsgObject);
-      // this.SocketService.SendChatMessage(chatMsgObject)
-      this.pushToChatWindow(chatMsgObject)
-      
-
-    }
-    else{
-      this.toastr.warning('text message can not be empty')
-
-    }
-
-  } // end sendMessage
-
-  public pushToChatWindow : any =(data)=>{
-
-    this.messageText="";
-    this.messageList.push(data);
-    this.scrollToChatTop = false;
-
-
-  }// end push to chat window
-
-  public getMessageFromAUser :any =()=>{
-
-      // this.SocketService.chatByUserId(this.userInfo.userId)
-      // .subscribe((data)=>{
-       
-
-        // (this.receiverId==data.senderId)?this.messageList.push(data):'';
-
-        // this.toastr.success(`${data.senderName} says : ${data.message}`)
-
-        // this.scrollToChatTop=false;
-
-      // }); end subscribe
-
-  }// end get message from a user 
-
-
-  public logout: any = () => {
-
-    this.AppService.logout()
-      .subscribe((apiResponse) => {
-
-        if (apiResponse.status === 200) {
-          console.log("logout called")
-          this.cookieService.delete('authtoken');
-
-          this.cookieService.delete('receiverId');
-
-          this.cookieService.delete('receiverName');
-
-          // this.SocketService.exitSocket()
-
-          this.router.navigate(['/']);
-
-        } else {
-          this.toastr.error(apiResponse.message)
-
-        } // end condition
-
-      }, (err) => {
-        this.toastr.error('some error occured')
-
-
-      });
-
-  } // end logout
 }
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -10,66 +10,100 @@ import { catchError, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HttpErrorResponse, HttpParams } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class SocketService {
 
   private url = 'https://chatapi.edwisor.com';
+
   private socket;
 
-  constructor( public http: HttpClient) 
-  { // connection is beiong create/handshake is happening
+
+  constructor(public http: HttpClient) {
+    // connection is being created.
+    // that handshake
     this.socket = io(this.url);
+
   }
 
-  // events to be listened
-  public verifyUser = () => {
-    return Observable.create((observer) => {
-      this.socket.on('verifyUser', (data) => {
-        observer.next(data);
-      }); //end socket
-    }); //end observable
-  } // end disconnect socket
+  // events to be listened 
   
+  public verifyUser = () => {
+
+    return Observable.create((observer) => {
+
+      this.socket.on('verifyUser', (data) => {
+
+        observer.next(data);
+
+      }); // end Socket
+
+    }); // end Observable
+
+  } // end verifyUser
 
   public onlineUserList = () => {
+
     return Observable.create((observer) => {
+
       this.socket.on("online-user-list", (userList) => {
+
         observer.next(userList);
-      }); //end socket
-    }); //end Observable
-  } // end online user list
+
+      }); // end Socket
+
+    }); // end Observable
+
+  } // end onlineUserList
+
 
   public disconnectedSocket = () => {
+
     return Observable.create((observer) => {
-      this.socket.on('disconnect', () => {
+
+      this.socket.on("disconnect", () => {
+
         observer.next();
-      }); // end socket
-    }); //end observable 
-  } // end disconnect socket
+
+      }); // end Socket
+
+    }); // end Observable
+
+
+
+  } // end disconnectSocket
 
   // end events to be listened
 
-  // events to be emitted start
+  // events to be emitted
+
   public setUser = (authToken) => {
+
     this.socket.emit("set-user", authToken);
-  } // end set user
-  // events to be emitted end
+
+  } // end setUser
+
+  // events to be emitted
+
 
   private handleError(err: HttpErrorResponse) {
+
     let errorMessage = '';
 
-    if (err.error instanceof Error)
-    {
-      errorMessage = `An error occured: ${err.error.message}`;
-    }
-    else
-    {
+    if (err.error instanceof Error) {
+
+      errorMessage = `An error occurred: ${err.error.message}`;
+
+    } else {
+
       errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-    }
-    console.log(errorMessage);
+
+    } // end condition *if
+
+    console.error(errorMessage);
 
     return Observable.throw(errorMessage);
-  } // end ErrorHandler
+
+  }  // END handleError
+
 }
